45e90430cb898c6d351654910a3ac6da78fcdce3
Brystol <odido160@gmail.com>
2023-11-19 18:59:57 +0100

    Searching products by name

--- src/app/modules/core/services/tutorials.service.ts
+++ src/app/modules/core/services/tutorials.service.ts
@@ -18,10 +18,19 @@ export class TutorialsService {
   getTutorials(
     pageIndex = 1,
     itemsPerPage = 5,
+    name: string | null = null,
   ): Observable<GetTutorialResponse> {
     let params = new HttpParams()
       .append('_page', pageIndex)
       .append('_limit', itemsPerPage);
+
+    //todo trzeba dodać enkodowanie url bo może nie poradzić sobie z polskimi znakami
+    if (name) {
+      //todo jednak z tym nie dzialalo do konca dobrze
+
+      // const encodedName = encodeURIComponent(name);
+      params = params.append('name_like', name);
+    }
 
     return this.httpClient
       .get<SimpleTutorial[]>(`${this.apiUrl}`, {
@@ -30,6 +39,9 @@ export class TutorialsService {
       })
       .pipe(
         map((response) => {
+          // if (name === '') {
+          //   return { tutorials: [], totalCount: 0 };
+          // }
           if (!response.body) return { tutorials: [], totalCount: 0 };
           const totalCount = Number(response.headers.get('X-Total-Count'));
 
--- src/app/modules/tutorial/components/tutorials/tutorials.component.ts
+++ src/app/modules/tutorial/components/tutorials/tutorials.component.ts
@@ -1,21 +1,39 @@
-import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { TutorialsService } from '../../../core/services/tutorials.service';
 import { SimpleTutorial } from '../../../core/models/tutorial/tutorial.models';
 import { MatPaginator } from '@angular/material/paginator';
-import { map, startWith, Subscription, switchMap } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Observable,
+  of,
+  startWith,
+  Subscription,
+  switchMap,
+} from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-tutorials',
   templateUrl: './tutorials.component.html',
   styleUrls: ['./tutorials.component.scss'],
 })
-export class TutorialsComponent implements AfterViewInit, OnDestroy {
+export class TutorialsComponent implements OnInit, AfterViewInit, OnDestroy {
   //after view init zeby pobrac z szablonu html mat paginator
   simpleTutorials: SimpleTutorial[] = [];
   totalCount = 0;
   sub$ = new Subscription();
-  tutorialsErrorMessage: string | null = null;
+  searchControl = new FormControl<string>('');
+  filteredOptions!: Observable<SimpleTutorial[]>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -23,7 +41,27 @@ export class TutorialsComponent implements AfterViewInit, OnDestroy {
     private tutorialService: TutorialsService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
+    private notifierService: NotifierService,
   ) {}
+
+  ngOnInit(): void {
+    this.filteredOptions = this.searchControl.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((value) => {
+        //return this.tutorialService.getTutorials(1, 10, value);
+        if (value!.trim() !== '') {
+          return this.tutorialService.getTutorials(1, 10, value);
+        } else {
+          // Jeśli wartość pola jest pusta, zwróć pustą listę
+          return of({ tutorials: [] });
+        }
+      }),
+      map(({ tutorials }) => {
+        return [...tutorials];
+      }),
+    );
+  }
 
   ngAfterViewInit(): void {
     this.tutorialService.getTutorials().subscribe({
@@ -41,8 +79,16 @@ export class TutorialsComponent implements AfterViewInit, OnDestroy {
           const itemsPerPage = queryMap.get('ilosc')
             ? Number(queryMap.get('ilosc'))
             : this.paginator.pageSize;
+
+          const tutorialName = queryMap.get('name')
+            ? queryMap.get('name')
+            : null;
           //todo mniej wiecej tutaj dodac spinner do ladowania produktow, tutaj zaczyna sie pobieranie
-          return this.tutorialService.getTutorials(page, itemsPerPage);
+          return this.tutorialService.getTutorials(
+            page,
+            itemsPerPage,
+            tutorialName,
+          );
         }),
         map(({ tutorials, totalCount }) => {
           this.totalCount = totalCount;
@@ -52,7 +98,8 @@ export class TutorialsComponent implements AfterViewInit, OnDestroy {
       )
       .subscribe({
         error: (err) => {
-          this.tutorialsErrorMessage = err;
+          //todo te błędy err pobierają się poprawnie dzięki error handling interceptor
+          this.notifierService.notify('error', err);
         },
       });
 
@@ -78,7 +125,11 @@ export class TutorialsComponent implements AfterViewInit, OnDestroy {
 
             this.router.navigate([], {
               relativeTo: this.activatedRoute,
-              queryParams: { page: pageIndex, itemsPerPage: itemsPerPage },
+              queryParams: {
+                page: pageIndex,
+                itemsPerPage: itemsPerPage,
+                name: this.searchControl.value,
+              },
             });
           },
         }),
@@ -87,5 +138,18 @@ export class TutorialsComponent implements AfterViewInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.sub$.unsubscribe();
+  }
+
+  searchTutorials() {
+    this.paginator.pageIndex = 0;
+    this.paginator.pageSize = 5;
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: {
+        page: this.paginator.pageIndex + 1,
+        itemsPerPage: this.paginator.pageSize,
+        name: this.searchControl.value,
+      },
+    });
   }
 }
